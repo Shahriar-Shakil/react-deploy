9afe8ad21462b41cbd55eb49a7c1004c949b3573
shahriar-shakil <toshahriarshakil@gmail.com>
2020-06-15 17:14:24 +0600

    Updates

---
+++ precache-manifest.323d2f69fe700d97ac63277cc801a183.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5e71635b700e07f26e64ffa957ad83",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "3b6d963a540adc35161b",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "169db92b2963a491015e",
+    "url": "/react-deploy/static/js/2.904c5842.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.904c5842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b6d963a540adc35161b",
+    "url": "/react-deploy/static/js/main.c93d0fad.chunk.js"
+  },
+  {
+    "revision": "88cc752382718b1d2923",
+    "url": "/react-deploy/static/js/runtime-main.625baed9.js"
+  }
+]);
